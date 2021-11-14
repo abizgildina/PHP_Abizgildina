3d4ca737ef09946f7fba79f8410b44d20c5e52f7
abizgildina <39943314+abizgildina@users.noreply.github.com>
2021-11-14 20:36:01 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,30 +4,48 @@
 echo '<title>Абизгильдина Д.Р.</title>';
  $d=date("d.m.Y H:i");
  echo($d);
-  echo '<h2>Часть 1. Введение в РНР</h2>';
+  echo '<h1>Лабораторная работа №1</h1>';
+  echo '<h2>Часть 1. Введение в PHP</h2>';
  ?>
 
   <ul id = "menu">
 	<li> <a href="/"> Упражнение 1-1 </a> </li>
-	<li> <a href="/lab-1-1.php"> Упражнение 1-2 </a> </li>
-	<li> <a href="/lab-1-2.php"> Упражнение 1-3 </a> </li>
-	<li> <a href="/lab-1-3.php"> Упражнение 1-4 </a> </li>
-	<li> <a href="/lab-1-5.php"> Самостоятельная работа 1. Задача 1-1 </a> </li>
+	<li> <a href="/LR1/lab-1-1.php"> Упражнение 1-2 </a> </li>
+	<li> <a href="/LR1/lab-1-2.php"> Упражнение 1-3 </a> </li>
+	<li> <a href="/LR1/lab-1-3.php"> Упражнение 1-4 </a> </li>
+	<li> <a href="/LR1/lab-1-5.php"> Самостоятельная работа 1. Задача 1-1 </a> </li>
 </ul>
 
 <?php
   echo '<h2>Часть 2. Управляющие конструкции</h2>';
  ?>
   <ul id = "menu">
-	<li> <a href="/lab-2-1.php"> Упражнение 1-6 </a> </li>
-	<li> <a href="/lab-2-2.php"> Упражнение 1-7 </a> </li>
-	<li> <a href="/lab-2-3.php"> Упражнение 1-8 </a> </li>
-	<li> <a href="/lab-2-4.php"> Упражнение 1-9 </a> </li>
-	<li> <a href="/lab-2-5.php"> Упражнение 1-10 </a> </li>	
-	<li> <a href="/lab-2-6.php"> Упражнение 1-11 </a> </li>
-	<li> <a href="/lab-2-7.php"> Упражнение 1-12 </a> </li>
-	<li> <a href="/lab-2-8.php"> Упражнение 1-13 </a> </li>
-	<li> <a href="/lab-2-9.php"> Упражнение 1-14 </a> </li>	
-	<li> <a href="/lab-2-10.php"> Самостоятельная работа 2. Задача 1-2 </a> </li>
-    <li> <a href="/lab-2-11.php"> Самостоятельная работа 2. Задача 1-3 </a> </li>
+	<li> <a href="/LR1/lab-2-1.php"> Упражнение 1-6 </a> </li>
+	<li> <a href="/LR1/lab-2-2.php"> Упражнение 1-7 </a> </li>
+	<li> <a href="/LR1/lab-2-3.php"> Упражнение 1-8 </a> </li>
+	<li> <a href="/LR1/lab-2-4.php"> Упражнение 1-9 </a> </li>
+	<li> <a href="/LR1/lab-2-5.php"> Упражнение 1-10 </a> </li>	
+	<li> <a href="/LR1/lab-2-6.php"> Упражнение 1-11 </a> </li>
+	<li> <a href="/LR1/lab-2-7.php"> Упражнение 1-12 </a> </li>
+	<li> <a href="/LR1/lab-2-8.php"> Упражнение 1-13 </a> </li>
+	<li> <a href="/LR1/lab-2-9.php"> Упражнение 1-14 </a> </li>	
+	<li> <a href="/LR1/lab-2-10.php"> Самостоятельная работа 2. Задача 1-2 </a> </li>
+    <li> <a href="/LR1/lab-2-11.php"> Самостоятельная работа 2. Задача 1-3 </a> </li>
+</ul>
+<?php
+	echo '<h1>Лабораторная работа №2</h1>';
+	 echo '<h2>Часть 1. Работа с массивами</h2>';
+ ?>
+  <ul id = "menu">
+	<li> <a href="/LR2/lab-2-1.php"> Упражнение 2-1 </a> </li>
+	<li> <a href="/LR2/lab-2-2.php"> Упражнение 2-2 </a> </li>
+	<li> <a href="/LR2/lab-2-3.php"> Упражнение 2-3 </a> </li>
+	<li> <a href="/LR2/lab-2-4.php"> Самостоятельная работа 1. Задача 2-4 </a> </li>
+</ul>
+<?php
+  echo '<h2>Часть 2.Функции пользователя</h2>';
+ ?>
+  <ul id = "menu">
+	<li> <a href="/LR2/lab-2-5.php"> Упражнение 2-5 </a> </li>
+	<li> <a href="/LR2/lab-2-6.php"> Упражнение 2-6 </a> </li>
 </ul>
